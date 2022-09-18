196b34e1ea306574b4defa9f8a7ff7f7a75da9fa
Shaunak <shaunak241997@gmail.com>
2022-09-18 18:58:40 +0530

    Needle in haystack (Implement find method for string in python3)

---
+++ string/needle_haystack.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
+
+
+s = Solution()
+print(s.strStr('sadbutsad', 'sad'))
+print(s.strStr('leetcode', 'leeto'))
+print(s.strStr('mississippi', 'issip'))
