c5c8deafd8795ba5eec0e8de4d45eaefe822b61d
Shaunak <shaunak241997@gmail.com>
2022-09-22 23:29:13 +0530

    Reverse a string using two-pointer approach

---
+++ string/reverse_string_two_pointer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s) -> None:
+        '''
+        Do not return anything, modify s in-place instead.
+        '''
+        start, end = 0, len(s) - 1
+        while start <= end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
+
+s = Solution()
+print(s.reverseString(['h','e','l','l','o']))
+print(s.reverseString(['H','a','n','n','a','h']))
