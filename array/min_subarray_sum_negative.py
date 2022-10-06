fb0eef352d25b7e56d2b687014828da2ea69c074
Shaunak Thakar <36746155+shaunak24@users.noreply.github.com>
2022-10-06 16:59:15 +0530

    Min Length Subarray Sum (Negative Nos)

---
+++ array/min_subarray_sum_negative.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+# Time - O(N)
+# Space - O(N)
+
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        curr_sum, min_len = 0, float('inf')
+        q = deque()
+        q.append([0, 0])
+        for i, num in enumerate(nums):
+            curr_sum += num
+            #print('curr_sum', curr_sum, q[0][1])
+            while q and (curr_sum - q[0][1] >= k):
+                min_len = min(min_len, i - q.popleft()[0] + 1)
+                #print('q after compress', q)
+                #print('curr_sum after compress', curr_sum)
+                #print('min_len after compress', min_len)
+            while q and curr_sum <= q[-1][1]:
+                q.pop()
+                #print('q after pop', q)
+            q.append([i + 1, curr_sum])
+            #print('q after iteration', q)
+        return min_len if min_len != float('inf') else -1
